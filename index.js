e8a15d62fa22e1f15cd143a214d7d64ba1f384f1
Edith Thaba <writersherry5@gmail.com>
2024-01-08 18:17:30 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) { // Assuming the game field is 400px wide (360 + 40 for dodger width)
+      dodger.style.left = `${left + 1}px`;
+    }
+}
